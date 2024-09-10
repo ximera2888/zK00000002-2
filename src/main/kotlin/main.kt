ea6def13544c6e6b83e02b70a7f99a55c4597458
ximera2888 <ximera2888@gmail.com>
2024-09-10 21:57:18 +0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main(
+
+) {
+    val likes=21
+    var n:Char
+    n=likes.toString().last()
+    if (n=='1'){println("Понравилось "+ likes+" человеку")}else(println("Понравилось "+ likes+" людям"))
+}
